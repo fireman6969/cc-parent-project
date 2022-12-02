da498c57dad20c405dffe1fb1d1d84a03745b746
fima <108056814+fireman6969@users.noreply.github.com>
2022-12-02 14:39:41 +0400

    first project

---
+++ parentProj/assets/Script/functionScript.ts
@@ -0,0 +1,19 @@
+
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+ 
+@ccclass('functionScript')
+export class functionScript extends Component {
+   
+    
+    public read() : void {
+        
+    }
+
+    public write() : void {
+        
+    }
+}
+
+
